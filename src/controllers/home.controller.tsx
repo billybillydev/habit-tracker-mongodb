39d864aa6968cca22bdf3b43587e05ee2014fd93
believelody <believelody@gmail.com>
2024-07-11 03:18:17 +0200

    implemented production environment

--- src/controllers/home.controller.tsx
+++ src/controllers/home.controller.tsx
@@ -6,6 +6,5 @@ import { HomePage } from "$pages/home.page";
 export const homeController = new Hono<{ Variables: AppVariables }>()
   .use(isAuthMiddleware)
   .get("/", ({ var: { isAuth }, html }) => {
-    console.log("isAuth in home:", isAuth);
     return html(<HomePage isAuth={isAuth} />);
   });
